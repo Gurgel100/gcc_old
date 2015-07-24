f122dccd427f8182f8a09ad5f5b7f3f7212447dd
Gurgel100 <baesae@hotmail.ch>
2015-07-25 01:46:41 +0200

    Changed startfile_spec and endfile_spec to exclude crtbegin.o and crtend.o

--- gcc/config/youros.h
+++ gcc/config/youros.h
@@ -5,6 +5,14 @@
 /* Default arguments you want when running your x86_64-youros-gcc toolchain */
 #define LIB_SPEC "-lc -lg -lm" /* link against C standard libraries */
 
+/* Provide start of programm */
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "crt0.o%s"
+
+/* Provide end of programm */
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC ""
+
 /* Don't automatically add extern "C" { } around header files. */
 #undef NO_IMPLICIT_EXTERN_C
 #define NO_IMPLICIT_EXTERN_C 1
